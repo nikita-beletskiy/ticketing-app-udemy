b0fb515c5205692775c4328db9a701d214fd9255
Nikita Beletskiy <bel.nikita3197@gmail.com>
2020-12-24 18:41:23 +0300

    Complete auth service
    All route handlers of the auth service are complete. Fixed a bug inside Password.compare() function. Added 3 utility middlewares for request validation and current user verification. Also added an error handler that invokes when verification is failed.

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -13,8 +13,8 @@ export class Password {
 
   static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.');
-    const buffer = (await scryptAsync(hashedPassword, salt, 64)) as Buffer;
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
-    return buffer.toString('hex') === suppliedPassword;
+    return buffer.toString('hex') === hashedPassword;
   }
 }
