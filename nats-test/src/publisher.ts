b45a5bfbdf7c4a29e31082e4557908427814bb22
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-01-13 17:26:32 +0300

    Added NATS Streaming Server test implementation
    Folder nats-test contains basic logic for NATS Streaming Server event bus, which is going to be used in this app later on (code will be relocated into common module and regular app files).

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,20 @@
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+console.clear();
+
+const stan = nats.connect('ticketing', 'abc', { url: 'http://localhost:4222' });
+
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
+
+  try {
+    await new TicketCreatedPublisher(stan).publish({
+      id: '123',
+      title: 'concert',
+      price: 20
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
