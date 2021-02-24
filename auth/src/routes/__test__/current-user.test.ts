b83e44913402ae4f544613233dfbb7a0e7b65ba7
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-02-24 15:47:26 +0300

    Fixing test commit

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -11,7 +11,7 @@ it('responds with details about the current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(400);
+    .expect(200);
 
   expect(response.body.currentUser.email).toEqual(testEmail);
 });
